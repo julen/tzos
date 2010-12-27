c1298dfd145402276ce3bfa17790016b3d936f82
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-12-27 22:21:50 +0100

    Use templating for the index page.
    This adds a basic layout for the site.

--- tzos/views/frontend.py
+++ tzos/views/frontend.py
@@ -8,10 +8,10 @@
     :copyright: (c) 2011 Julen Ruiz Aizpuru.
     :license: BSD, see LICENSE for more details.
 """
-from flask import Module
+from flask import Module, render_template
 
 frontend = Module(__name__)
 
 @frontend.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html')

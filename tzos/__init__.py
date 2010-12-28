ed8d24cc3e1dca8a6b645d11a4d5180a43aa1a1c
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-12-28 12:40:57 +0100

    Use Flask-Babel for i18n/l10n purposes.
    Strings are marked for translation and a toy PO file has been included.

--- tzos/__init__.py
+++ tzos/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flaskext.babel import Babel
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.cfg')
+    app.babel = Babel(app)
 
     from tzos.views.frontend import frontend
     app.register_module(frontend)

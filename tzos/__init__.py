a735833db27b6923f4e5a4cd409f875693e81f7a
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-12-28 10:41:22 +0100

    Read settings from configuration file.

--- tzos/__init__.py
+++ tzos/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile('settings.cfg')
 
     from tzos.views.frontend import frontend
     app.register_module(frontend)

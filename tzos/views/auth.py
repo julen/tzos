0fecab078eefce878f46e23fc369193493b207b8
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-12-29 19:16:54 +0100

    Force trailing slash.

--- tzos/views/auth.py
+++ tzos/views/auth.py
@@ -12,10 +12,10 @@ from flask import Module, render_template
 
 auth = Module(__name__)
 
-@auth.route('/register')
+@auth.route('/register/')
 def register():
     return render_template('auth/register.html')
 
-@auth.route('/login')
+@auth.route('/login/')
 def login():
     return render_template('auth/login.html')

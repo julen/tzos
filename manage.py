7aa1327291b972045b1b1c1e5130db53ba57f9d8
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-01-10 18:08:11 +0100

    New management commands: createall and dropall.
    'createall' creates the DB schema and 'dropall' drops all the data
    that is available in the DB.

--- manage.py
+++ manage.py
@@ -10,11 +10,21 @@
 """
 from flask import current_app
 
-from flaskext.script import Manager
+from flaskext.script import Manager, prompt_bool
 
 from tzos import create_app
+from tzos.extensions import db
 
 manager = Manager(create_app)
+
+@manager.command
+def createall():
+    db.create_all()
+
+@manager.command
+def dropall():
+    if prompt_bool('Are you sure? This will delete all the data.'):
+        db.drop_all()
 
 @manager.shell
 def make_shell_context():

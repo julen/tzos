5a4feda2098ab8737f97e3e78967289bcd84fc9b
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-01-26 13:25:44 +0100

    List terms by letter.

--- tzos/views/terms.py
+++ tzos/views/terms.py
@@ -17,7 +17,7 @@ from tzos.extensions import dbxml
 terms = Module(__name__)
 
 @terms.route('/')
-def terms_list():
+def list_all():
     terms_list = []
 
     qs = 'collection()/martif/text/body/termEntry/langSet[@xml:lang="%s"]/tig/term/string()' % (g.dict)
@@ -28,5 +28,20 @@ def terms_list():
         cur = terms.next()
         terms_list.append(cur.asString().decode('utf-8'))
 
-    return render_template('terms/terms_list.html', terms=terms_list)
+    return render_template('terms/list_all.html', terms=terms_list)
 
+@terms.route('/0-9/')
+@terms.route('/<string(length=1):letter>/')
+def list_letter(letter='0-9'):
+    letter = str(letter)
+    terms_list = []
+
+    qs = 'collection()/martif/text/body/termEntry/langSet[@xml:lang="%s"]/tig/term[starts-with(string(), "%s")]/string()' % (g.dict, letter)
+    terms = dbxml.query(qs)
+
+    while terms.hasNext():
+        cur = terms.next()
+        terms_list.append(cur.asString().decode('utf-8'))
+
+    return render_template('terms/list_letter.html', terms=terms_list,
+                                                     letter=letter)

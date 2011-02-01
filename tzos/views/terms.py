b7b4d5b19bcd938044e0e70dc82b9ff91bb74d45
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-02-01 19:05:21 +0100

    Simplify code by using new facilities of Flask-DBXML.

--- tzos/views/terms.py
+++ tzos/views/terms.py
@@ -16,30 +16,18 @@ terms = Module(__name__)
 
 @terms.route('/')
 def list_all():
-    terms_list = []
-
     qs = '/martif/text/body/termEntry/langSet[@xml:lang="%s"]/tig/term/string()' % (g.dict)
-    terms = g.dbxml.query(qs)
+    terms = g.dbxml.query(qs).as_str().all()
     #terms = dbxml.query("distinct-values(collection()/martif/text/body/termEntry/langSet/@xml:lang)")
 
-    while terms.hasNext():
-        cur = terms.next()
-        terms_list.append(cur.asString().decode('utf-8'))
-
-    return render_template('terms/list_all.html', terms=terms_list)
+    return render_template('terms/list_all.html', terms=terms)
 
 @terms.route('/0-9/')
 @terms.route('/<string(length=1):letter>/')
 def list_letter(letter='0-9'):
     letter = str(letter)
-    terms_list = []
 
     qs = '/martif/text/body/termEntry/langSet[@xml:lang="%s"]/tig/term[starts-with(string(), "%s")]/string()' % (g.dict, letter)
-    terms = g.dbxml.query(qs)
+    terms = g.dbxml.query(qs).as_str().all()
 
-    while terms.hasNext():
-        cur = terms.next()
-        terms_list.append(cur.asString().decode('utf-8'))
-
-    return render_template('terms/list_letter.html', terms=terms_list,
-                                                     letter=letter)
+    return render_template('terms/list_letter.html', terms=terms, letter=letter)

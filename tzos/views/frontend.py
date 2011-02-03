53263c2a6b07d8554773a77e1ffeb51df47ef4eb
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-02-03 12:13:15 +0100

    Language selection page for non-js clients.

--- tzos/views/frontend.py
+++ tzos/views/frontend.py
@@ -8,7 +8,13 @@
     :copyright: (c) 2011 Julen Ruiz Aizpuru.
     :license: BSD, see LICENSE for more details.
 """
-from flask import Module, render_template
+from flask import Module, flash, redirect, render_template, request
+
+from flaskext.babel import gettext as _
+
+from tzos.helpers import url_for
+
+from babel import Locale
 
 frontend = Module(__name__)
 
@@ -19,4 +25,13 @@ def index():
 
 @frontend.route('/<lang>/dict/')
 def dict():
+    newdict = request.args.get('setdict', None)
+
+    if newdict:
+        # FIXME: maybe a helper function could get locale display names
+        flash(_(u"From now on your dictionary language is ‘%(dictname)s’.",
+                dictname=Locale.parse(newdict).display_name.capitalize()))
+
+        return redirect(url_for('frontend.index'))
+
     return render_template('dict.html')

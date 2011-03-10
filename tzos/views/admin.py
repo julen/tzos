caf23c78f6437e8daa21403258ab961aade5bb9a
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-03-10 17:51:05 +0100

    Display users with privileged permissions in the user permissions tab.

--- tzos/views/admin.py
+++ tzos/views/admin.py
@@ -15,6 +15,7 @@ from flaskext.babel import gettext as _
 from tzos.extensions import db
 #from tzos.forms import EditPermissionsForm
 from tzos.helpers import url_for
+from tzos.models import User
 from tzos.permissions import admin as admin_permission
 
 admin = Module(__name__)
@@ -23,24 +24,6 @@ admin = Module(__name__)
 @admin.route("/", methods=("GET", "POST"))
 @admin_permission.require(401)
 def settings():
-    #permissionform = EditPermissionForm(obj=g.user)
+    users = User.query.filter(User.role > User.MEMBER)
 
-    allowed_actions = {
-        #'editpermission': permissionform,
-        }
-    action = request.args.get('action', None)
-
-    form = None
-    if action and action in allowed_actions:
-        form = allowed_actions[action]
-
-    if form and form.validate_on_submit():
-        form.populate_obj(g.user)
-        db.session.commit()
-
-        flash(_("Your account has been updated."), "success")
-
-        return redirect(url_for("admin.settings"))
-
-    return render_template("admin/settings.html")
-                           #permissionform=permissionform)
+    return render_template("admin/settings.html", users=users)

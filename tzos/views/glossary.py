bb9294ecb2322ff8457fa456833a02a1f81507fe
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-03-30 14:27:50 +0200

    Don't list all the glossary entries.

--- tzos/views/glossary.py
+++ tzos/views/glossary.py
@@ -16,13 +16,6 @@ from tzos.extensions import dbxml
 
 glossary = Module(__name__)
 
-@glossary.route('/')
-def list_all():
-    qs = '/martif/text/body/termEntry/langSet[@xml:lang="%s"]/tig/term/string()' % (session['tzos_dict'])
-    terms = dbxml.get_db().query(qs).as_str().all()
-
-    return render_template('glossary/list_all.html', terms=terms)
-
 @glossary.route('/<dict>/<string(length=1):letter>/')
 def list_letter(dict, letter):
 

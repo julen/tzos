1348324db0acc99eba03e845704677003b6e45bc
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-04-13 10:43:13 +0200

    Add spacing for readability.

--- tzos/static/tzos.js
+++ tzos/static/tzos.js
@@ -2,9 +2,11 @@ $(document).ready(function () {
     $('#tzosDict > a').click(function (e) {
         e.preventDefault();
     });
+
     $('#tzosDict').hover(function () {
         $(this).children('ul').toggle();
     });
+
     $('ul.inlineTabs').tabs();
 
     $('.showme').show();

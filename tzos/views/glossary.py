aa10a7748a4995e003bdd0d62c9bd01c5a6dd779
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-05-09 20:39:39 +0200

    Ok, it's safe to just set an empty string rather than strange things.

--- tzos/views/glossary.py
+++ tzos/views/glossary.py
@@ -22,7 +22,7 @@ glossary = Module(__name__)
 def list_letter(dict, letter):
 
     ctx = {'lang': dict, 'letter': letter,
-           'current_user': getattr(g.user, 'username', '__anon__')}
+           'current_user': getattr(g.user, 'username', '')}
 
     pn = int(request.args.get('p', 1))
 

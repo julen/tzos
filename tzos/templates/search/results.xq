78a774ed765f7f8af4a7469ffae7eb73ef7483cb
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-05-16 09:38:46 +0200

    Unify quick and advanced searches.

--- tzos/templates/search/results.xq
+++
@@ -1,5 +0,0 @@
-import module namespace term = "http://tzos.net/term" at "term.xqm";
-
-for $term in collection($collection)//term
-where dbxml:contains($term/string(), $q) and term:is_public($term)
-return term:asLink($term)
--- tzos/views/search.py
+++ tzos/views/search.py
@@ -18,35 +18,52 @@ from tzos.helpers import dropdown_list, get_dict_langs
 
 search = Module(__name__)
 
+def _get_search_param(key):
+    val = request.args.get(key, '')
+
+    if val and val == 'all':
+        val = ''
+
+    return val
+
+def _get_search_filters():
+
+    filter = "true() "
+
+    lang = _get_search_param('lang')
+
+    if lang:
+        filter += 'and $term/../..[@xml:lang="{0}"]'.format(lang)
+
+    return filter
+
 @search.route('/')
 def quick():
+    page = None
     q = request.args.get('q', '').strip()
 
-    if not q:
-        return redirect(url_for('search.advanced'))
-
-    pn = int(request.args.get('p', 1))
-    ctx = {'q': q}
-
-    page = dbxml.get_db().template_query('search/results.xq',
-                                         context=ctx).as_rendered(). \
-                                         paginate(pn, 10, error_out=False)
-
-    return render_template('search/results.html', q=q, page=page)
-
-@search.route('/advanced/', methods=('GET', 'POST',))
-def advanced():
-    page = None
-    form = SearchForm()
-
-    form.language.choices = dropdown_list(get_dict_langs(), 'all', _('All'))
-
-    if form.validate_on_submit():
+    if q:
         pn = int(request.args.get('p', 1))
-        ctx = {'q': form.keywords.data}
 
-        page = dbxml.get_db().template_query('search/results.xq',
-                                             context=ctx).as_rendered(). \
-                                             paginate(pn, 10, error_out=False)
+        filter = _get_search_filters()
+        print filter
+        #ctx = {'q': q, 'filter': _get_search_filters() }
 
-    return render_template('search/advanced.html', form=form, page=page)
+        qs = """
+        import module namespace term = "http://tzos.net/term" at "term.xqm";
+
+        for $term in collection($collection)//term
+        where dbxml:contains($term/string(), "{0}") and
+              term:is_public($term) and {1}
+        return term:asLink($term)
+        """.format(q, filter)
+        print qs
+
+        page = dbxml.get_db().raw_query(qs).as_str(). \
+                                            paginate(pn, 10, error_out=False)
+
+    form = SearchForm(request.args, csrf_enabled=False)
+
+    form.lang.choices = dropdown_list(get_dict_langs(), 'all', _('All'))
+
+    return render_template('search/results.html', form=form, q=q, page=page)

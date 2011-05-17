460f2cfa541b1c27bda0aeb582a3f243635d469a
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-05-17 16:58:39 +0200

    Update TODO comment.

--- tzos/views/search.py
+++ tzos/views/search.py
@@ -62,7 +62,7 @@ def _get_search_filters():
         ('lang', '$term/../..[@xml:lang="{0}"]'),
         ('subject_field', '$term/../../../descrip[@type="subjectField"]/string() = "{0}"'),
         ('product_subset', '$term/../admin[@type="productSubset"]/string() = "{0}"'),
-        # TODO: Concept origin, product subset
+        # TODO: Concept origin
         ('na', '$term/../termNote[@type="normativeAuthorization"]/string() = "{0}"'),
         ('na_org', '$term/../termNote[@type="normativeAuthorization"][@target="{0}"]'),
         ('pos', '$term/../termNote[@type="partOfSpeech"]/string() = "{0}"'),

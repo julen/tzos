fb5ed68c5bca261ecb9f66261f1f9af2214017aa
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-05-17 09:18:01 +0200

    Add an extra 'extract' step for finer granularity.

--- gettext.sh
+++ gettext.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 usage="\
-Usage: $0 <update|compile>
+Usage: $0 <extract|update|compile>
 
 Extract or compile gettext messages."
 
 if [ -z "$1" ]; then
     echo "$usage"
-elif [ $1 == "update" ]; then
-    echo "Updating POT template…"
+elif [ $1 == "extract" ]; then
+    echo "Extracting messages into the POT template…"
     pybabel extract -F configs/babel.cfg -o tzos/translations/messages.pot tzos/ -c l10n -k _l
-    echo
+elif [ $1 == "update" ]; then
     echo "Updating languages from the latest template…"
     pybabel update -i tzos/translations/messages.pot -d tzos/translations/
 elif [ $1 == "compile" ]; then

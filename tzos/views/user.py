ee8dc0b3ae922c16f07016195108d04fb482bb15
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-06-07 17:37:15 +0200

    Display latest user activity on public profile pages.

--- tzos/views/user.py
+++ tzos/views/user.py
@@ -10,7 +10,8 @@
 """
 from flask import Module, render_template
 
-from tzos.models import Comment, User
+from tzos.extensions import dbxml
+from tzos.models import Comment, TermChange, User
 
 user = Module(__name__)
 
@@ -20,5 +21,27 @@ def profile(username):
 
     user_comments = Comment.query.filter_by(author=user).order_by(Comment.date_created.desc())[0:5]
 
+    qs = '''
+    import module namespace term = "http://tzos.net/term" at "term.xqm";
+    let $txs :=
+        for $tx in collection($collection)//tig/transacGrp
+        let $tig := $tx/..
+        where term:is_public($tig) and
+              $tx/transacNote[@type="responsibility"]/string()="{0}"
+        order by $tx/date descending
+        return string-join(
+            ($tig/data(@id),
+            $tig/term/string(),
+            $tx/transac[@type="transactionType"]/string(),
+            $tx/date/string(),
+            $tx/transacNote[@type="responsibility"]/string()
+            ), "|||")
+    return subsequence($txs, 1, 5)
+    '''.format(username.encode('utf-8'))
+
+    user_activity = \
+            dbxml.get_db().raw_query(qs).as_callback(TermChange.parse).all()
+
     return render_template('user/profile.html', user=user,
+                                                user_activity=user_activity,
                                                 user_comments=user_comments)

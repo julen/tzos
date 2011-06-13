b7d8937c0bdd3e49f5740593a2ff1e1089f8bcbe
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-06-13 19:57:43 +0200

    New field available: SelectFieldPlus.
    This is a SelectField that can be sorted and set default placeholder
    values. It is possible to define exceptions when sorting by passing a
    tuple of option names to `no_sort` in the field constructor.

--- tzos/forms/fields.py
+++ tzos/forms/fields.py
@@ -8,10 +8,13 @@
     :copyright: (c) 2011 Julen Ruiz Aizpuru.
     :license: BSD, see LICENSE for more details.
 """
+from operator import itemgetter
+
 from flask import g
 
 from flaskext.babel import lazy_gettext as _
 from flaskext.wtf import BooleanField, SelectField, TextField
+
 
 class DynamicSelectField(SelectField):
     """A SelectField that allows disabling pre validation, which complicates
@@ -31,6 +34,57 @@ class DynamicSelectField(SelectField):
                     break
             else:
                 raise ValueError(_(u'Not a valid choice'))
+
+
+class SelectFieldPlus(SelectField):
+    """A SelectField which can be sorted and set default placeholder values."""
+
+    PLACEHOLDERS = {
+        '': u'----',
+        'all': _(u'All'),
+    }
+
+    def __init__(self, label=None, validators=None, coerce=unicode, \
+            choices=None, placeholder=None, sort=False, \
+            no_sort=(), **kwargs):
+
+        self.placeholder = placeholder
+        self.sort = sort
+        self.sort_exceptions = no_sort
+
+        super(SelectFieldPlus, self).__init__(label, validators, \
+                coerce, choices, **kwargs)
+
+    def iter_choices(self):
+        """Do some black magic before yelding values."""
+
+        no_sort = []
+
+        if self.sort_exceptions and self.sort:
+
+            for ex in self.sort_exceptions:
+                try:
+                    i = map(itemgetter(0), self.choices).index(ex)
+                    no_sort.insert(0, self.choices.pop(i))
+                except ValueError:
+                    pass
+
+        if self.sort:
+            self.choices = sorted(self.choices, key=lambda x: x[1])
+
+        # If there are exceptions we don't want to sort, insert them
+        # at the beginning of the choices list
+        map(lambda x: self.choices.insert(0, x), no_sort)
+
+        try:
+            value = self.PLACEHOLDERS[self.placeholder]
+        except KeyError:
+            value = None
+
+        if value:
+            self.choices.insert(0, (self.placeholder, value))
+
+        return super(SelectFieldPlus, self).iter_choices()
 
 
 class BooleanWorkingField(BooleanField):

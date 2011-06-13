21b5b0a4e563543ee50a19e721553bf4b348d073
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-06-13 14:45:00 +0200

    Cache latest comments and activity in the front page.

--- tzos/views/frontend.py
+++ tzos/views/frontend.py
@@ -14,7 +14,7 @@ from flaskext.babel import gettext as _
 
 from babel import Locale
 
-from tzos.extensions import dbxml
+from tzos.extensions import cache, dbxml
 from tzos.forms import SearchForm
 from tzos.helpers import anon_cached, dropdown_list, get_dict_langs
 from tzos.models import Comment, TermChange
@@ -27,23 +27,33 @@ def index():
     form = SearchForm()
     form.lang.choices = dropdown_list(get_dict_langs(), 'all', _(u'All'))
 
-    latest_comments = Comment.query.order_by(Comment.date_created.desc())[0:5]
+    latest_comments = cache.get("latest_comments")
 
-    qs = '''
-    import module namespace term = "http://tzos.net/term" at "term.xqm";
-    let $txs :=
-        for $tx in collection($collection)//tig/transacGrp
-        let $tig := $tx/..
-        where term:is_public($tig)
-        order by $tx/date descending
-        return $tx
-    for $tx in subsequence($txs, 1, 5)
-    return term:activity($tx)
-    '''
+    if latest_comments is None:
+        latest_comments = Comment.query. \
+                order_by(Comment.date_created.desc())[0:5]
 
-    # TODO: cache latest activity
-    latest_activity = \
-            dbxml.session.raw_query(qs).as_callback(TermChange.parse).all()
+        cache.set("latest_comments", latest_comments)
+
+    latest_activity = cache.get("latest_activity")
+
+    if latest_activity is None:
+        qs = '''
+        import module namespace term = "http://tzos.net/term" at "term.xqm";
+        let $txs :=
+            for $tx in collection($collection)//tig/transacGrp
+            let $tig := $tx/..
+            where term:is_public($tig)
+            order by $tx/date descending
+            return $tx
+        for $tx in subsequence($txs, 1, 5)
+        return term:activity($tx)
+        '''
+
+        latest_activity = dbxml.session. \
+                raw_query(qs).as_callback(TermChange.parse).all()
+
+        cache.set("latest_activity", latest_activity)
 
     return render_template('index.html', form=form,
                                          latest_activity=latest_activity,

a69b05085a12239dd243381a061e5e13a6339406
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-06-13 12:40:42 +0200

    Use unicode strings.

--- tzos/forms/account.py
+++ tzos/forms/account.py
@@ -18,106 +18,106 @@ from tzos.models import User
 USERNAME_RE = r'^[\w.+-]+$'
 
 is_username = regexp(USERNAME_RE,
-                     message=_("You can only use letters, numbers or dashes"))
+                     message=_(u"You can only use letters, numbers or dashes"))
 
 
 class LoginForm(Form):
     next = HiddenField()
 
-    login = TextField(_('Username or email address'), validators=[
+    login = TextField(_(u'Username or email address'), validators=[
                       required(message=\
-                               _('You must provide an email or username.'))])
+                               _(u'You must provide an email or username.'))])
 
-    password = PasswordField(_('Password'))
+    password = PasswordField(_(u'Password'))
 
-    remember = BooleanField(_('Remember me'))
+    remember = BooleanField(_(u'Remember me'))
 
-    submit = SubmitField(_('Login'))
+    submit = SubmitField(_(u'Login'))
 
 
 class SignupForm(Form):
     next = HiddenField()
 
-    username = TextField(_("Username"), validators=[
-                         required(message=_("Username is required.")),
+    username = TextField(_(u"Username"), validators=[
+                         required(message=_(u"Username is required.")),
                          is_username])
 
-    password = PasswordField(_("Password"), validators=[
-                             required(message=_("Password is required."))])
+    password = PasswordField(_(u"Password"), validators=[
+                             required(message=_(u"Password is required."))])
 
-    password_repeat = PasswordField(_("Repeat Password"), validators=[
+    password_repeat = PasswordField(_(u"Repeat Password"), validators=[
                                     equal_to("password", message=\
-                                             _("Passwords don't match"))])
+                                             _(u"Passwords don't match"))])
 
-    email = TextField(_("Email address"), validators=[
-                      required(message=_("Email address is required.")),
-                      email(message=_("A valid email address is required."))])
+    email = TextField(_(u"Email address"), validators=[
+                      required(message=_(u"Email address is required.")),
+                      email(message=_(u"A valid email address is required."))])
 
-    recaptcha = RecaptchaField(_("Copy the words appearing below"))
+    recaptcha = RecaptchaField(_(u"Copy the words appearing below"))
 
-    submit = SubmitField(_("Signup"))
+    submit = SubmitField(_(u"Signup"))
 
 
     def validate_username(self, field):
         user = User.query.filter(User.username.like(field.data)).first()
         if user:
-            raise ValidationError, gettext("This username is taken.")
+            raise ValidationError, gettext(u"This username is taken.")
 
     def validate_email(self, field):
         user = User.query.filter(User.email.like(field.data)).first()
         if user:
-            raise ValidationError, gettext("This email is taken.")
+            raise ValidationError, gettext(u"This email is taken.")
 
 
 class RecoverPasswordForm(Form):
-    email = TextField(_("Your email address"), validators=[
-                      email(message=_("A valid email address is required."))])
+    email = TextField(_(u"Your email address"), validators=[
+                      email(message=_(u"A valid email address is required."))])
 
-    submit = SubmitField(_("Recover password"))
+    submit = SubmitField(_(u"Recover password"))
 
 
 class BasePasswordForm(Form):
-    password = PasswordField(_("Password"), validators=[
-                             required(message=_("Password is required."))])
+    password = PasswordField(_(u"Password"), validators=[
+                             required(message=_(u"Password is required."))])
 
-    password_again = PasswordField(_("Repeat password"), validators=[
+    password_again = PasswordField(_(u"Repeat password"), validators=[
                                    equal_to("password", message=\
-                                            _("Passwords don't match."))])
+                                            _(u"Passwords don't match."))])
 
 class ResetPasswordForm(BasePasswordForm):
     activation_key = HiddenField()
 
-    submit = SubmitField(_("Reset password"))
+    submit = SubmitField(_(u"Reset password"))
 
 
 class EditPasswordForm(BasePasswordForm):
-    submit = SubmitField(_("Change password"))
+    submit = SubmitField(_(u"Change password"))
 
 
 class EditEmailForm(Form):
-    email = TextField(_("Your email address"), validators=[
-                      required(message=_("Email address required.")),
-                      email(message=_("A valid email address is required."))])
+    email = TextField(_(u"Your email address"), validators=[
+                      required(message=_(u"Email address required.")),
+                      email(message=_(u"A valid email address is required."))])
 
-    submit = SubmitField(_("Edit email address"))
+    submit = SubmitField(_(u"Edit email address"))
 
     def validate_email(self, field):
         user = User.query.filter(User.email.like(field.data)).first()
 
         if user:
-            raise ValidationError, gettext("This email is taken.")
+            raise ValidationError, gettext(u"This email is taken.")
 
 
 class EditProfileForm(Form):
-    display_name = TextField(_("Display name"))
+    display_name = TextField(_(u"Display name"))
 
-    website = TextField(_("Website"), validators=[
+    website = TextField(_(u"Website"), validators=[
                         Optional(),
-                        URL(message=_("The URL must be valid."))])
+                        URL(message=_(u"The URL must be valid."))])
 
-    company = TextField(_("Company/Organization"))
+    company = TextField(_(u"Company/Organization"))
 
-    location = TextField(_("Location"))
+    location = TextField(_(u"Location"))
 
-    submit = SubmitField(_("Update information"))
+    submit = SubmitField(_(u"Update information"))
 

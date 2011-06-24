45f12b8ceacc8fa00bb7d4bdb99e1e912001696a
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-06-24 10:29:35 +0200

    Don't list user's private terms in the glossary.

--- tzos/views/glossary.py
+++ tzos/views/glossary.py
@@ -29,13 +29,12 @@ def list_letter(dict, letter):
 
     for $tig in collection($collection)/martif/text/body/termEntry/langSet[@xml:lang="{0}"]/tig
     where $tig[starts-with(lower-case(term/string()), "{1}")] and
-          (term:is_public($tig) or term:owner($tig) = "{2}")
+          term:is_public($tig)
     order by lower-case($tig/term/string()) ascending,
              $tig/term/string() descending
     return term:values($tig)
     """.format(dict.encode('utf-8'),
-               letter.encode('utf-8'),
-               getattr(g.user, 'username', u'').encode('utf-8'))
+               letter.encode('utf-8'))
 
     page = dbxml.session.raw_query(qs). \
             as_callback(Term.parse).paginate(pn, 10)

46965bbe885454e09b52db75584f67fb548a340b
Julen Ruiz Aizpuru <julenx@gmail.com>
2011-09-19 17:27:16 +0200

    Edit settings for LaTeX output.

--- docs/conf.py
+++ docs/conf.py
@@ -173,16 +173,26 @@ htmlhelp_basename = 'TZOSdoc'
 
 # The paper size ('letter' or 'a4').
 #latex_paper_size = 'letter'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
 #latex_font_size = '10pt'
+latex_font_size = '15pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'TZOS.tex', u'TZOS Documentation',
+   ('index', 'TZOS.tex', u'TZOS: Dokumentazio Teknikoa',
    u'Julen Ruiz Aizpuru', 'manual'),
 ]
+
+latex_use_modindex = False
+latex_additional_files = ['flaskstyle.sty']
+
+latex_elements = {
+    'preamble':     r'\usepackage{flaskstyle}\selectlanguage{basque}'
+}
+latex_use_parts = False
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
